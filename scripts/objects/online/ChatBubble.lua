a7f2317a3f1c537e6801cda102bd666e95f829a0
hyperboid <hyperboid@nullanoid.net>
2024-10-22 01:57:05 -0400

    "Properly" calculate ChatBubble size (it's still a little dumb)

--- scripts/objects/online/ChatBubble.lua
+++ scripts/objects/online/ChatBubble.lua
@@ -1,52 +1,36 @@
 ---@class ChatBubble: Object
 local ChatBubble, super = Class(Object)
 
-
-local function physicalStrlen(a)
-    local b = ""
-    for i, str in ipairs(Utils.splitFast( string.gsub(a,"]","["), "[")) do
-        if i % 2 == 1 then
-            b = b .. str
-        end
-    end
-    return string.len(b)
-end
-
 function ChatBubble:init(actor, text, x, y)
     super.init(self,x,y)
-    local split_text = Utils.split(text, "\n")
-    local longest_line = 0
-    for _, v in ipairs(split_text) do
-        longest_line = math.max(longest_line, physicalStrlen(v))
-    end
-    self.origin_x = 0.5 -- * longest_line
-    self.width = (16 * longest_line) + 4
-    self.height = (28 * #split_text) + 8
-    self.x_offset = actor.width
-    self.y_offset = -actor.height
-    self.text = Text(text, self.x_offset, self.y_offset, self.width, self.height, {
+    self.origin_x = 0.5
+    local x_offset = actor.width * 2
+    local y_offset = -actor.height
+    self.text = Text(text, x_offset, y_offset, SCREEN_WIDTH * 2, nil, {
         align = "center",
         font = "main_mono"
     })
+    self.width, self.height = self.text:getSize()
+    self.text.width, self.text.height = self.text:getSize() -- Freeze!
+    self.text:setText(text)
     self.lifetime = 11
+    self.rectangle = Rectangle(x_offset, y_offset, self.text:getSize())
+    self:addChild(self.rectangle)
     self:addChild(self.text)
+    self.rectangle.alpha = 0.5
+    self.rectangle.color = {0.2,0.2,0.2}
 end
 
 function ChatBubble:update()
+    super.update(self)
     self.lifetime = self.lifetime - DT
     if self.lifetime < 0 then return self:remove() end
     if self.lifetime < 1 then
         self.alpha = self.lifetime
         self.text.alpha = self.lifetime
+        self.rectangle.alpha = self.lifetime * 0.5
     end
 end
 
-function ChatBubble:draw()
-    love.graphics.push("all")
-    love.graphics.setColor(0.3,0.3,0.3,0.5 * self.alpha)
-    love.graphics.rectangle("fill", self.x_offset, self.y_offset, self.width, self.height)
-    love.graphics.pop()
-    super.draw(self)
-end
 
 return ChatBubble
